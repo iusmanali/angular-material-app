d7bd876e282d83affaacc7d80c7d1f189fb055e2
iusmanali <usmanmunnawar@gmail.com>
2020-11-25 00:24:30 +0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
+import Keyboard from "simple-keyboard";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: "./app.component.html",
+  styleUrls: [
+    "../../node_modules/simple-keyboard/build/css/index.css",
+    "./app.component.scss"
+  ]
 })
 export class AppComponent {
-  title = 'angular-material-app';
+  // npm install simple-keyboard --save
+  showKeyBoard = true;
+  value = "";
+  keyboard: Keyboard;
+  ngAfterViewInit() {
+    this.keyboard = new Keyboard({
+      onChange: input => this.onChange(input),
+      onKeyPress: button => this.onKeyPress(button),
+      mergeDisplay: true,
+      layoutName: "numbers",
+      layout: {
+        default: [
+          "q w e r t y u i o p",
+          "a s d f g h j k l",
+          "{shift} z x c v b n m {backspace}",
+          "{numbers} {space} {ent}"
+        ],
+        shift: [
+          "Q W E R T Y U I O P",
+          "A S D F G H J K L",
+          "{shift} Z X C V B N M {backspace}",
+          "{numbers} {space} {ent}"
+        ],
+        numbers: ["1 2 3", "4 5 6", "7 8 9", "{close} 0 {backspace}"]
+      },
+      display: {
+        "{numbers}": "123",
+        "{ent}": "return",
+        "{escape}": "esc ⎋",
+        "{tab}": "tab ⇥",
+        "{backspace}": "⌫",
+        "{capslock}": "caps lock ⇪",
+        "{shift}": "⇧",
+        "{controlleft}": "ctrl ⌃",
+        "{controlright}": "ctrl ⌃",
+        "{altleft}": "alt ⌥",
+        "{altright}": "alt ⌥",
+        "{metaleft}": "cmd ⌘",
+        "{metaright}": "cmd ⌘",
+        "{abc}": "ABC",
+        "{close}": "Close"
+      }
+    });
+  }
+
+  onChange = (input: string) => {
+    this.value = input;
+    console.log("Input changed", input);
+  };
+
+  onKeyPress = (button: string) => {
+    console.log("Button pressed", button);
+    if(button === "{close}" || button === 'close') {
+      this.showKeyBoard = false;
+    }
+    if (button === "{shift}" || button === "{lock}") this.handleShift();
+    if (button === "{numbers}" || button === "{abc}") this.handleNumbers();
+  };
+
+  onInputChange = (event: any) => {
+    this.keyboard.setInput(event.target.value);
+  };
+
+  handleShift = () => {
+    let currentLayout = this.keyboard.options.layoutName;
+    let shiftToggle = currentLayout === "default" ? "shift" : "default";
+
+    this.keyboard.setOptions({
+      layoutName: shiftToggle
+    });
+  };
+
+  handleNumbers() {
+    let currentLayout = this.keyboard.options.layoutName;
+    let numbersToggle = currentLayout !== "numbers" ? "numbers" : "default";
+  
+    this.keyboard.setOptions({
+      layoutName: numbersToggle
+    });
+  }
+
+  ShowCustomKeyboad(event) {
+    this.showKeyBoard = !this.showKeyBoard;
+  }
 }
